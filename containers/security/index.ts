86a59da1765e8d72ff593b6340bdc66f429fe26e
Marco <marco.martinoli@protonmail.com>
2021-01-20 13:01:46 +0100

    Key Transparency

--- containers/security/index.ts
+++ containers/security/index.ts
@@ -1,3 +1,4 @@
 export { default as ExternalPGPSettingsSection } from './ExternalPGPSettingsSection';
 export { default as AddressVerificationSection } from './AddressVerificationSection';
+export { default as KeyTransparencySection } from './KeyTransparencySection';
 export { default as PGPSchemeSelect } from './PGPSchemeSelect';
