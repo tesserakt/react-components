2fb72cfa76b8cab6da453a8501e5d52b818c59bd
Marco <marco.martinoli@protonmail.com>
2021-01-27 12:33:49 +0100

    Add self-audit result to settings

--- containers/security/index.ts
+++ containers/security/index.ts
@@ -1,4 +1,3 @@
 export { default as ExternalPGPSettingsSection } from './ExternalPGPSettingsSection';
 export { default as AddressVerificationSection } from './AddressVerificationSection';
-export { default as KeyTransparencySection } from './KeyTransparencySection';
 export { default as PGPSchemeSelect } from './PGPSchemeSelect';
